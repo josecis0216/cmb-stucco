73fcb6e124bd40e16d455eae910e94c9b4dc9c54
josecis0216 <josecis0216@gmail.com>
2026-07-15 15:34:18 -0600

    added hero section, need to configure

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,5 @@
+export default function Projects() {
+    return <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <p>Projects page</p>
+    </div>
+};
